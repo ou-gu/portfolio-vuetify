8d01cb4f7dcee72213ef3c5838a1553ac0e9270d
ou-gu <wangbball@gmail.com>
2020-12-15 18:21:55 +0900

    外枠を作りました。

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,108 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer app v-model="drawer" clipped>
+      <v-container>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title grey--text text--darken-2">
+              Nabigation Lists
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list nav>
+          <v-list-group
+            v-for="nav_list in nav_lists"
+            :key="nav_list.name"
+            :prepend-icon="nav_list.icon"
+            no-action
+            :append-icon="nav_list.lists ? undefined : ''"
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item v-for="list in nav_list.lists" :key="list">
+              <v-list-item-content>
+                <v-list-item-title>{{ list }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+      </v-container>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar color="primary" dark app clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-items>
+        <v-btn text>For Enterprise</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" text>Support<v-icon>mdi-menu-down</v-icon></v-btn>
+          </template>
+          <v-list>
+            <v-subheader>Get help</v-subheader>
+            <v-list-item v-for="support in supports" :key="support.name">
+              <v-list-item-icon>
+                <v-icon>{{ support.icon }}</v-icon>
+                <v-list-item-content>
+                  <v-list-item-title>{{ support.name }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item-icon>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar-items>
     </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-footer color="primary" dark app> Vuetiry </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      drawer: null,
+      supports: [
+        { name: "Consulting and suppourt", icon: "mdi-vuetify" },
+        { name: "Discord community", icon: "mdi-discord" },
+        { name: "Report a bug", icon: "mdi-bug" },
+        { name: "Github issu board", icon: "mdi-github" },
+        { name: "Stack overview", icon: "mdi-stack-overflow" },
+      ],
+      nav_lists: [
+        {
+          name: "Getting Started",
+          icon: "mdi-speedometer",
+          lists: ["Quick Start", "Pre-made layouts"],
+        },
+        {
+          name: "Customization",
+          icon: "mdi-cogs",
+        },
+        {
+          name: "Styles & animations",
+          icon: "mdi-palette",
+          lists: ["Colors", "Content", "Display"],
+        },
+        {
+          name: "UI Components",
+          icon: "mdi-view-dashboard",
+          lists: ["API explorer", "Alerts"],
+        },
+        {
+          name: "Directives",
+          icon: "mdi-function",
+        },
+        {
+          name: "Preminum themes",
+          icon: "mdi-vuetify",
+        },
+      ],
+    };
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
